1b475413f921fa0fb6fe5ba8540bd8832544cf7b
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-07-15 15:15:22 +0200

    feat: header demo

--- components/header.tsx
+++ components/header.tsx
@@ -1,27 +1,48 @@
 import Image from 'next/image'
+import { TImageFlipper as TImageSetup } from '../types/common';
 
+
+const imagesLeft: Array<TImageSetup> = [
+  {
+    path: '/leg.svg',
+  },
+  {
+    path: '/sit.svg',
+  }
+];
+const imagesRight: Array<TImageSetup> = [
+  {
+    path: '/sit.svg',
+  },
+  {
+    path: '/leg.svg',
+  }
+];
 const Header = () => {
   return (
-    <div className="flex justify-between">
-      <div className="h-32 w-96 relative">
-        <Image
-          src="/leg.svg"
-          alt="Picture of the author"
-          layout="fill"
-          objectFit="contain"
-          className=""
-        />
-      </div>
-      <h1 className="text-5xl ">Matinka</h1>
-      <div className="h-64 w-96 relative">
-        <Image
-          src="/leg.svg"
-          alt="Picture of the author"
-          layout="fill"
-          objectFit="contain"
-          className="scale-x-[-1]"
-        />
-      </div>
+    <div className="flex justify-between mt-3">
+      {imagesLeft.map(e =>
+        <div className="h-32 w-96 relative">
+          <Image
+            src={e.path}
+            alt="Picture of the author"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+      )}
+      <h1 className="text-5xl pt-9">Matinka</h1>
+      {imagesRight.map(e =>
+        <div className="h-32 w-96 relative">
+          <Image
+            src={e.path}
+            alt="Picture of the author"
+            layout="fill"
+            objectFit="contain"
+            className="scale-x-[-1]"
+          />
+        </div>
+      )}
     </div>
   );
 }
---
+++ types/common.ts
@@ -0,0 +1,3 @@
+export type TImageFlipper = {
+  path: string;
+};
